71bd5aa2a07077b041253c448cc22eb4a6571bc0
Kaining <kainingchen2013@gmail.com>
2023-09-05 11:34:47 -0400

    List all entires and CSS

--- src/components/Entries.js
+++ src/components/Entries.js
@@ -4,6 +4,8 @@ import React, { useState, useEffect } from "react";
 
 const Entries = () => {
     const [entries, setEntries] = useState([]);
+    const [totalHours, setTotalHours] = useState(0);
+    const [totalAmount, setTotalAmount] = useState(0);
     const [loading, setLoading] = useState(true);
 
     const formatAmount = new Intl.NumberFormat("en-US", {
@@ -16,6 +18,25 @@ const Entries = () => {
         const res = await fetch("/api/entries");
         const newEntries = await res.json();
         setEntries(newEntries);
+
+        const totalBillableHours = newEntries.reduce((total, entry) => {
+            if (entry.billable === "Yes") {
+                return total + entry.hours;
+            } else {
+                return total;
+            }
+        }, 0);
+        setTotalHours(totalBillableHours.toFixed(2));
+
+        const totalBillableAmount = newEntries.reduce((total, entry) => {
+            if (entry.billable === "Yes") {
+                return total + (entry.billable_rate * entry.hours);
+            } else {
+                return total;
+            }
+        }, 0);
+        setTotalAmount(formatAmount.format(totalBillableAmount));
+
         setLoading(false);
     };
 
@@ -26,6 +47,17 @@ const Entries = () => {
     if (loading) return <h1>Loading...</h1>;
 
     return (
+        <>
+        <div className="header-container">
+            <div className="hours-tracked">
+                <h3 className="header-h3">Hours Tracked</h3>
+                <h1>{totalHours}</h1>
+            </div>
+            <div className="billable-amount">
+                <h3 className="header-h3">Billable Amount</h3>
+                <h1>{totalAmount}</h1>
+            </div>
+        </div>
         <table>
             <thead>
                 <tr>
@@ -39,22 +71,22 @@ const Entries = () => {
             <tbody>
                 {entries.map(entry => (
                     <tr key={entry.id}>
-                        <td>{entry.first_name} {entry.last_name}</td>
+                        <td>{entry.project}</td>
                         <td>{entry.client}</td>
                         <td>{entry.hours}</td>
                         {(entry.billable === "Yes") ?
                             (<td>{entry.hours} (100%)</td>) :
                             (<td>0.00 (0%)</td>)
                         }
-                        {
-                            (formatAmount.format(entry.billable_rate * entry.hours) !== "$0.00") ?
-                                (<td>{formatAmount.format(entry.billable_rate * entry.hours)}</td>) :
-                                (<td>-</td>)
+                        {(entry.billable === "Yes") ?
+                            (<td>{formatAmount.format(entry.billable_rate * entry.hours)}</td>) :
+                            (<td>-</td>)
                         }
                     </tr>
                 ))}
             </tbody>
         </table>
+        </>
     );
 }
 
